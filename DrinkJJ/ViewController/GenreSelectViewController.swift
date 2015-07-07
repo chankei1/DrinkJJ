a7dc288f431508784d81e2bc4544f5ad743dc5ee
chankei1 <kkeisukee222@gmail.com>
2015-07-07 18:03:03 +0900

    TopViewのボタンとgenreSelectの画面を大まかに作成

--- DrinkJJ/ViewController/GenreSelectViewController.swift
+++ DrinkJJ/ViewController/GenreSelectViewController.swift
@@ -6,4 +6,65 @@
 //  Copyright (c) 2015年 Keisuke.K. All rights reserved.
 //
 
-import Foundation
+import UIKit
+class GenreSelectViewController: UIViewController{
+    
+    //全てのジャンルを設置
+    let genreMenu: [String] = ["ビール", "SD", "サワー", "リキュール", "ウイスキー", "酒", "焼酎", "ワイン"]
+    let rect = UIScreen.mainScreen().bounds;
+    //スクリーンの幅
+    let screenWidth = Int( UIScreen.mainScreen().bounds.size.width);
+    //スクリーンの高さ
+    let screenHeight = Int(UIScreen.mainScreen().bounds.size.height);
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        println(rect)
+        println(screenWidth)//幅
+        println(screenHeight)//高さ
+        
+        //ジャンルの設置
+        setView()
+        
+        //タップ処理
+        //touchGenre()
+    }
+    
+    
+    
+    //ジャンルを8種類を設置する
+    func setView(){
+        
+        var calLabel: [UILabel] = []
+        for(var i = 0; i<8; i++){
+            calLabel.insert(UILabel(), atIndex: i)
+            calLabel[i] = UILabel(frame: CGRectMake(0,0,80,80))
+            calLabel[i].text = genreMenu[i]
+            calLabel[i].backgroundColor = UIColor.whiteColor()
+            calLabel[i].textAlignment = NSTextAlignment.Center
+            calLabel[i].layer.position = CGPoint(x: screenWidth/3+(i%2*100), y: screenHeight/6+(i/2*100))
+            calLabel[i].userInteractionEnabled = true;
+            calLabel[i].font = UIFont(name:"HelveticaNeue-Bold",size:20)
+            calLabel[i].tag = i+1
+            calLabel[i].numberOfLines = 0;
+            calLabel[i].font = UIFont.systemFontOfSize(12);//文字サイズ
+            calLabel[i].textAlignment = NSTextAlignment.Center//センター揃え
+            //calLabel[i].sizeToFit();
+            calLabel[i].layer.masksToBounds = true
+            calLabel[i].layer.cornerRadius = 40.0
+            self.view.addSubview(calLabel[i])
+        }
+    }
+    
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        println("ラベルを押しました")
+        
+        
+    }
+    
+    
+    
+}
--- DrinkJJ/ViewController/TopViewController.swift
+++ DrinkJJ/ViewController/TopViewController.swift
@@ -6,4 +6,48 @@
 //  Copyright (c) 2015年 Keisuke.K. All rights reserved.
 //
 
-import Foundation
+import UIKit
+class TopViewController: UIViewController, UIScrollViewDelegate{
+    
+    //画面サイズ
+    let screenRotationWidth = UIScreen.mainScreen().bounds.size.width
+    let screenRotationHeight = UIScreen.mainScreen().bounds.size.height
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        start()
+
+    }
+    
+
+    func start(){
+        // ボタンを生成する.
+        let nextButton: UIButton = UIButton(frame: CGRectMake(0, 0, screenRotationWidth/2, screenRotationHeight/10))
+        nextButton.backgroundColor = UIColor.redColor();
+        nextButton.layer.masksToBounds = true
+        nextButton.setTitle("ドリンクを作る", forState: .Normal)
+        nextButton.layer.cornerRadius = 20.0
+        nextButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height/1.5)
+        nextButton.addTarget(self, action: "onClickMyButton", forControlEvents: .TouchUpInside)
+        
+        // ボタンを追加する.
+        self.view.addSubview(nextButton);
+        
+    }
+    
+    
+    func onClickMyButton(){
+        // 遷移するViewを定義する.
+        let genreSelectViewController: UIViewController = GenreSelectViewController()
+        
+        // アニメーションを設定する.
+        genreSelectViewController.modalTransitionStyle = UIModalTransitionStyle.PartialCurl
+        
+        // Viewの移動する.
+        self.presentViewController(genreSelectViewController, animated: true, completion: nil)
+        
+    }
+
+}
